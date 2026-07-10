06fbf104661bc31ab0d65d4a7fa7fdde445e2392
themastersheep <176472302+themastersheep@users.noreply.github.com>
2026-07-10 09:07:39 +0100

    fix(nvim): drop mini.notify

--- nvim/.config/nvim/plugin/30_mini.lua
+++ nvim/.config/nvim/plugin/30_mini.lua
@@ -64,26 +64,6 @@ now_if_args(function()
 end)
 
 -- ----------------------------------------------------------------------------
--- mini.notify
--- ----------------------------------------------------------------------------
-
-later(function()
-    vim.pack.add({ "https://github.com/nvim-mini/mini.notify" })
-
-    local win_config = function()
-        local has_statusline = vim.o.laststatus > 0
-        local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
-        return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
-    end
-    local notify = require("mini.notify")
-    notify.setup({
-        lsp_progress = { enable = false },
-        window       = { config = win_config() },
-    })
-    vim.notify = notify.make_notify()
-end)
-
--- ----------------------------------------------------------------------------
 -- mini.diff
 -- ----------------------------------------------------------------------------
 
